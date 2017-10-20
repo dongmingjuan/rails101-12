2f082ffc5efe94cbbb3967d4402e4a3e1025b384
dongmingjuan <mingjuandong@mingdeMacBook-Pro.local>
2017-10-20 09:31:00 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,7 +26,7 @@ class GroupsController < ApplicationController
   # POST /groups.json
   def create
     @group = Group.new(group_params)
-    @group.user = current_user
+    @group.user = current_user 
 
     respond_to do |format|
       if @group.save
