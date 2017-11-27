44daec2a94c83147b9a2754b148b5b24d16fabee
dongmingjuan <https://github.com/dongmingjuan>
2017-11-27 15:46:35 +0800

    change the group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -91,6 +91,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:title, :description)
+      params.require(:group).permit(:title, :description, :released_on)
     end
 end
